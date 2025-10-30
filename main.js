1467b64536e66fad3c122d432a5e9ac50734f20b
elecinas <estherlecina@gmail.com>
2025-10-30 09:49:30 +0100

    Instala electron

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  // Carga index.html local
+  win.loadFile(path.join(__dirname, "index.html"));
+
+  // Abre las herramientas de desarrollo (opcional)
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
